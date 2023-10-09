aca0176adb2632c02512e5c94986cf6a1baa2179
AlinaTantsura <alinatanst@gmail.com>
2023-10-09 21:01:46 +0300

    Task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+btnStop.setAttribute('disabled', 'disabled');
+btnStart.addEventListener('click', startTimer);
+btnStop.addEventListener('click', stopTimer);
+
+function startTimer() {
+   timerId  = setInterval(changeBackgroundColor, 1000);
+    btnStart.setAttribute('disabled', 'disabled');
+    btnStop.removeAttribute('disabled');
+}
+function changeBackgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+function stopTimer() {
+    clearInterval(timerId);
+    btnStart.removeAttribute('disabled');
+    btnStop.setAttribute('disabled', 'disabled');
+}

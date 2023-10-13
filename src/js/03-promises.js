0154c32293b4f3e0649f1fc9830542c7a177cb9a
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-10-13 20:45:28 +0300

    Task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', handlerSubmit);
@@ -13,35 +14,20 @@ function handlerSubmit(event) {
     delayInput += stepInput;
   }
 }
-// function createPromise(position, delay) {
-  
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//     if (shouldResolve) {
-//       // Fulfill
-//       resolve(console.log(`${position} promise is resolve`));
-//     } else {
-//       // Reject
-//       reject(console.log(`${position} promise is reject`));
-//     }
-//     }, delay);
-//   })
-//   return promise;
-// }
-// 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({position: position, delay: delay});
+      } else {
+        // Reject
+        reject({position: position, delay: delay});
+      }
+    }, delay);
+  }).then((obj) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+  .catch((obj) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
+}

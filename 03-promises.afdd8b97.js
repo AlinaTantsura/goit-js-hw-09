cfc381d1d57b1a9cbc8ba160e8f7184ff413d5f5
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-10-13 17:45:50 +0000

    Deploying to gh-pages from @ AlinaTantsura/goit-js-hw-09@0154c32293b4f3e0649f1fc9830542c7a177cb9a 🚀

--- 03-promises.afdd8b97.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");e.addEventListener("submit",(function(t){t.preventDefault();let n=Number(e.elements.delay.value);const u=Number(e.elements.step.value),l=Number(e.elements.amount.value);for(let e=0;e<l;e++)createPromise(e+1,n),n+=u}));
-//# sourceMappingURL=03-promises.afdd8b97.js.map
